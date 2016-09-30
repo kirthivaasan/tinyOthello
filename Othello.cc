3839d4c3cad313e2e38f2a1a77d07cd7df8b4f53
Kirthivaasan Puniamurthy <kirthivaasanp@gmail.com>
2016-09-30 12:37:00 +0800

    Add files via upload
    shortened flip

--- Othello.cc
+++ Othello.cc
@@ -5,13 +5,11 @@
 class Othello {
 	public:
 		Othello() {
-			for (int i = 0; i < 8; i++) for (int j = 0; j < 8; j++) board[i][j] = 0;	// zeros out board array.
+			for (int i = 0; i < 8; i++) for (int j = 0; j < 8; j++) board[i][j] = 0;	// zeroes out board array.
 			board[3][3] =  board[4][4] = -1;
 			board[4][3] =  board[3][4] = 1;
 		}
-		bool place(int player, char col, int row) {
-			return makeMove(player, charToInt(col), 8 - row);
-		}
+		bool place(int player, char col, int row) { return makeMove(player, charToInt(col), 8 - row); }
 		bool ai(int player) {
 			for (int i = 0; i < 8; ++i) for (int j = 0; j < 8; ++j) if (makeMove(player, i, j)) return true;
 			return false;
@@ -42,7 +40,7 @@ class Othello {
 	private:
 		bool makeMove(int player,int col, int row) {
 			if ( inBounds(row, col) && board[row][col] == 0) { // check bounds and if spot is empty.
-				if ( flip(player, row, col) ) {
+				if (flip(player, row, col) ) {
 					board[row][col] = player;
 					return true;
 				}
@@ -50,16 +48,15 @@ class Othello {
 			return false;
 		}
 		bool inBounds(int row, int col) { return row >= 0 && row < 8 && col >= 0 && col < 8; }
-		bool flip(int player, int row, int col, int d_row, int d_col, bool playerNeighbourCheck=false) {
-			row += d_row;
-			col += d_col;
-			int current = board[row][col];
-			if (!inBounds(row, col) || current == 0)		return false;						// current is out of bounds or blank
-			if (current == player && !playerNeighbourCheck)	return true;						// current is player piece but not original player pos
-			if (current == -1*player && flip(player, row, col, d_row, d_col, false)) {	// current has to be opp piece, call flipHelper.
+		bool flip(const int player, int row, int col, const int d_row, const int d_col, bool isNeighbour=true) {
+			row += d_row; col += d_col;
+			if (!inBounds(row, col)) return false;
+			if (board[row][col] == player && !isNeighbour) return true;	// current is player piece and not a direct neighbour of the current move.
+			if (board[row][col] == -1*player && flip(player, row, col, d_row, d_col, false)) {	// current has to be opp piece, call flipHelper.
 				board[row][col] = player;
 				return true;
 			}
+			return false;
 		}
 		bool flip(int player, int r, int c) {
 			bool valid[8] = { flip(player, r, c, -1, 0), flip(player, r, c,  1, 0), flip(player, r, c, 0, -1), flip(player, r, c, -1, 1),
